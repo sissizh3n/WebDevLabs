b85cc42b009727dd46cde19586d5a718489d5d86
sissizh3n <szhen.zyx@gmail.com>
2025-03-20 00:55:42 -0400

    final updates to lab 5: location

--- static/script.js
+++ static/script.js
@@ -81,29 +81,23 @@ function getAdvice() {
         });
 }
 
-// Initialize and add the map
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
+  const position = { lat: 40.443490, lng: -79.941643 };
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
     zoom: 10,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Pittsburgh",
   });
 }
 
